b7d268d630caf4ebd4e990307ea73e137dc560eb
hermes76 <44623908+hermes76@users.noreply.github.com>
2020-09-11 14:24:41 -0500

    Add files via upload

--- Sorting and Searching/SubarraySumsI.cpp
+++ Sorting and Searching/SubarraySumsI.cpp
@@ -1,45 +1,29 @@
-#include <iostream>
+//#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
+    //freopen("A.txt","r",stdin);
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
     int n,m;
     cin>>n>>m;
     int arr[n];
-    for(int x=0; x<n; x++)
-    {
-        cin>>arr[x];
-    }
-    long long cuenta=0;
-    int indice=0;
+    for(int x =0; x<n; x++)cin>>arr[x];
+    int suma=0;
     int res=0;
-    while(cuenta<m and indice<n)
-    {
-        cuenta+=arr[indice];
-        indice++;
-    }
-    if(cuenta==m)
-    {
-        res++;
-    }
+    int indice1=0;
     int indice2=0;
-    while(indice2+1<n)
+    for(indice2; indice2<n;)
     {
-        if(cuenta>=m)
-        {
-        cuenta-=arr[indice2];
-        indice2++;
-        }
-
-        if(cuenta<m and indice<n)
-        {
-            cuenta+=arr[indice];
-            indice++;
-        }
-        if(cuenta==m)
-        {
-            res++;
-        }
+        if(suma+arr[indice2]<=m){suma+=arr[indice2]; indice2++;}
+        else break;
+    }
+    while(indice1<n)
+    {
+        if(suma==m)res++;
+        if((indice2<n) and (suma+arr[indice2]<=m)){suma+=arr[indice2]; indice2++;}
+        else {suma-=arr[indice1]; indice1++;}
     }
     cout<<res;
 }
